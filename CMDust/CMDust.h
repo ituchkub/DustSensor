360e5cdf9d524535fade525db340360819af629b
ituchkub <ituchkub@hotmail.com>
2015-07-08 19:29:04 +0700

    Update CMDust.h

--- CMDust/CMDust.h
+++ CMDust/CMDust.h
@@ -8,7 +8,7 @@ class Dust {
  private:
 uint8_t pin;
 byte _buff[2];
-int pinDust = 14;//DSM501A input D8
+
 unsigned long duration,starttime,endtime,sampletime_ms = 30000,lowpulseoccupancy = 0;
 float ratio = 0,concentrations = 0;
 int i=0;   
